e47356fe4be300c2879d4bfa8f9e86e4cd8bca4b
luattran <seldatSWT@gmail.com>
2019-04-01 08:39:28 +0700

    cap nhat giao thong, kiem tra lai chi so 255

--- SeldatUnilever_Ver1.02/Management/TrafficManager/TrafficRounterService.cs
+++ SeldatUnilever_Ver1.02/Management/TrafficManager/TrafficRounterService.cs
@@ -311,7 +311,7 @@ namespace SelDatUnilever_Ver1._00.Management.TrafficManager
             int index = -1;
             foreach (ZoneRegister z in ZoneRegisterList.Values)
             {
-                if (z.Index < 200)
+                if (z.Index < 255) // kiem tra lai số 225
                 {
                     if (ExtensionService.IsInPolygon(z.GetZone(), p))
                     {

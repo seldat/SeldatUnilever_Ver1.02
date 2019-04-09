1a0486e7b9ee41963389a79eed6cb6590483ac56
Phát Nguyễn <phatnguyen.pf@gmail.com>
2019-04-09 21:00:15 +0700

    update

--- SeldatUnilever_Ver1.02/Communication/HttpServerRounter/HttpServer.cs
+++ SeldatUnilever_Ver1.02/Communication/HttpServerRounter/HttpServer.cs
@@ -8,6 +8,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SelDatUnilever_Ver1._00.Communication
 {
@@ -22,9 +23,12 @@ namespace SelDatUnilever_Ver1._00.Communication
                 this.port = port;
             }
 
-            public void listen()
+        public void listen()
+        {
+
+            Task.Run(() =>
             {
-                Task.Run(() =>
+                try
                 {
                     listener = new TcpListener(port);
                     listener.Start();
@@ -35,9 +39,19 @@ namespace SelDatUnilever_Ver1._00.Communication
                         Thread thread = new Thread(new ThreadStart(processor.process));
                         thread.Start();
                         Thread.Sleep(1);
+
                     }
-                   });
-            }
+                }
+                catch
+                {
+                    MessageBox.Show("Chương Trình Khác Đã Mở !");
+                    Environment.Exit(0);
+                }
+            });
+                   
+    
+              
+         }
         public virtual void handleGETRequest(HttpProcessor p) { }
         public virtual void handlePOSTRequest(HttpProcessor p, StreamReader inputData) { }
     }
